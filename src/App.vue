132d4dc743f53fe9a8caeee135d97b5333e5fc9e
zhoufenglu <abindef@hotmail.com>
2022-11-02 09:47:23 +0800

    add button

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,32 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Component1></Component1>
+    <Component2 title="title1 :"></Component2>
+    <Component3></Component3>
+    <Component4></Component4>
+    <Component5></Component5>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Component1 from './components/Component1.vue'
+import Component2 from './components/Component2.vue'
+import Component3 from './components/Component3.vue'
+import Component4 from './components/Component4.vue'
+import Component5 from './components/Component5.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Component1,
+    Component2,
+    Component3,
+    Component4,
+    Component5
+  },
+  props:['title']
 }
 </script>
 
@@ -25,4 +40,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+margin: 10px;
+
+}
+
 </style>
---
+++ src/components/Component2.vue
@@ -0,0 +1,43 @@
+<!-- aaa -->
+<template>
+<div class='bbb'>
+    <button>父控件传参</button>
+</div>
+</template>
+
+<script>
+
+export default {
+name:'ButtonParm',
+components: {},
+data() {
+//这里存放数据
+return {
+
+};
+},
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+beforeCreate() {}, //生命周期 - 创建之前
+beforeMount() {}, //生命周期 - 挂载之前
+beforeUpdate() {}, //生命周期 - 更新之前
+updated() {}, //生命周期 - 更新之后
+beforeDestroy() {}, //生命周期 - 销毁之前
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script> 
